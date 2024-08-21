580369d1becf18731194e322b3cb5687fb771fad
Md. Touhidul Islam <110239106+touhid0503@users.noreply.github.com>
2024-08-21 16:06:26 +0600

    21-8-24

---
+++ SQLQuery1.sql
@@ -0,0 +1,30 @@
+create database touhid;
+use touhid;
+create table Employee(
+	WORKER_ID int primary key,
+	FIRST_NAME varchar(20),
+	LAST_NAME varchar(20),
+	SALARY int,
+	DEPT_NAME varchar(20)
+)
+insert into Employee(WORKER_ID,FIRST_NAME,LAST_NAME,SALARY,DEPT_NAME) 
+values(201,'Md Touhidul','Islam',550,'CSE')
+
+select * from Employee
+
+alter table Employee 
+add JOINING_DATE varchar(20)
+
+alter table Employee 
+alter column SALARY varchar(20)
+
+
+insert into Employee(WORKER_ID,FIRST_NAME,LAST_NAME,SALARY,DEPT_NAME) 
+values(202,'Md Touhidul','Islam','540','CSE')
+
+alter table Employee 
+alter column DEPT_NAME varchar(10)
+
+
+ 
+
